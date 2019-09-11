67a8e7bc0b82727b9e54720d459a85dd83ed96c1
Barbara Whiting <whitingba@gmail.com>
2019-09-10 19:54:32 -0600

    update the apiroutes to hopefully final

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,31 +1,12 @@
 // Routes
 // =============================================================
 
-// Basic route that sends the user first to the AJAX Page
-app.get("/", function (req, res) {
-    res.sendFile(path.join(__dirname, "view.html"));
+
+app.get("/tables", function (req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
 });
 
-app.get("/add", function (req, res) {
-    res.sendFile(path.join(__dirname, "add.html"));
+app.get("/reservations", function (req, res) {
+    res.sendFile(path.join(__dirname, "newres.html"));
 });
 
-// Displays all characters
-app.get("/api/characters", function (req, res) {
-    return res.json(characters);
-});
-
-// Displays a single character, or returns false
-app.get("/api/characters/:character", function (req, res) {
-    var chosen = req.params.character;
-
-    console.log(chosen);
-
-    for (var i = 0; i < characters.length; i++) {
-        if (chosen === characters[i].routeName) {
-            return res.json(characters[i]);
-        }
-    }
-
-    return res.json(false);
-})
